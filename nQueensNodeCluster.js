889cb006dbaa2f1bc7bec15a19bd0f45951fa6c7
natoen <hgmatsuda@gmail.com>
2016-08-09 23:56:37 +0900

    (refactor) Revise code

--- nQueensNodeCluster.js
+++ nQueensNodeCluster.js
@@ -1,66 +1,58 @@
 const cluster = require('cluster');
 const os = require('os');
-
-const board = Math.pow(2, 16) - 1;
+const board = Math.pow(2, 4) - 1;
 
 if (cluster.isMaster) {
   const timeStarted = Date.now();
   let position = ~(0 | 0 | 0) & board; // in base 2;
   let count = 0;
 
-  function currentBit() {
+  const currentBit = () => {
     const bit = position & -position;
     position = position ^ bit;
 
-    return bit;
+    return { minorD: bit << 1, column: bit, majorD: bit >> 1 };
   };
 
   for (let i = 0; i < os.cpus().length; i++) {
     if (position) {
       const worker = cluster.fork();
-      const bit = currentBit();
 
-      worker.send({minorD: bit << 1, column: bit, majorD: bit >> 1});
-
-      worker.on('message', (message) => {
-        if (message === 'solving' && position) {
-          const bit = currentBit();
-
-          worker.send({minorD: bit << 1, column: bit, majorD: bit >> 1});
-        } else if (message === 'solution') {
-          count++;
-        } else {
-          worker.kill();
-        }
+      worker.send(currentBit());
+      worker.on('message', message => {
+        if (message === 'solving' && position) worker.send(currentBit());
+        else if (message === 'solution') count++;
+        else worker.kill();
       });
-    } 
+    }
   }
-  
-  cluster.on('exit', function(worker, code, signal) {
-    if (!Object.keys(cluster.workers).length) {
+
+  cluster.on('exit', () => {
+    let disconnected = false;
+
+    if (!Object.keys(cluster.workers).length && !disconnected) {
       cluster.disconnect();
-      console.log(`We have ${count} solution${(count > 1 ? 's' : '')} found in ${(Date.now() - timeStarted).getSeconds()} secs.`);
+      disconnected = true;
+      const time = (Date.now() - timeStarted) / 1000;
+      console.log(`We have found ${count} solution${(count > 1 ? 's' : '')} time: ${time} seconds`);
     }
   });
 } else {
-  function solve(minorD, column, majorD) {
-    if (board === column) {
-      process.send('solution');
-    }
+  const solve = (minorD, column, majorD) => {
+    if (board === column) process.send('solution');
 
     let position = ~(minorD | column | majorD) & board;
-    
+
     while (position) {
       const bit = position & -position;
       position = position ^ bit;
-     
-      solve((minorD | bit) << 1, column | bit, (majorD | bit) >> 1); 
+
+      solve((minorD | bit) << 1, column | bit, (majorD | bit) >> 1);
     }
-  }
+  };
 
-  process.on('message', ({minorD, column, majorD}) => {
+  process.on('message', ({ minorD, column, majorD }) => {
     solve(minorD, column, majorD);
-
     process.send('solving');
   });
 }

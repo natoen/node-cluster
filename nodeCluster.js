cafafbbf599224c5922984b621fdeaabe2a2caa9
natoen <hgmatsuda@gmail.com>
2016-08-10 20:31:48 +0900

    (feat) Add simple node cluster I/O

---
+++ nodeCluster.js
@@ -0,0 +1,37 @@
+const cluster = require('cluster');
+const cpus = require('os').cpus().length;
+
+const getTime = () => {
+  const secs = (new Date).getSeconds();
+  const milli = (new Date).getMilliseconds();
+  return `${secs}.${milli}`;
+};
+
+if (cluster.isMaster) { // We start the workers here
+  for (let i = 0; i < cpus; i++) {
+    const worker = cluster.fork();
+
+    console.log(`Starting worker #${worker.process.pid} ${getTime()}`);
+
+    worker.on('message', message => { // listens for messages coming from worker
+      if (message === 'exit') {
+        worker.kill();
+        console.log(`Worker #${worker.process.pid} exiting ${getTime()}`);
+      }
+    });
+  }
+
+  // listener when a worker is 'fully' running
+  cluster.on('online', worker => {
+    console.log(`Worker #${worker.process.pid} online at ${getTime()}`);
+  });
+
+  // listener when a worker exits
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker #${worker.process.pid} exits with code: ${code} signal: ${signal}`);
+  });
+} else {
+  // we go here everytime we fork / a worker is online
+  console.log(`Worker #${process.pid} outside master ${getTime()}`);
+  process.send('exit');
+}
